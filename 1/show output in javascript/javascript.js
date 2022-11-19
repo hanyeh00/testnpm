ca265f967d75d3374b5e6c9edaafa2a8a4a768c8
hanieh <h.khaleoghli@gmail.com>
2022-11-19 23:19:42 +0330

    Add files via upload

---
+++ 1/show output in javascript/javascript.js
@@ -0,0 +1,13 @@
+alert("hello");
+
+console.log("welcome\t"+"to"+"the page"); //class console
+
+document.write("Alice\n"+"<br>"+"hi"); //class document
+document.write("<hr>"+"csv"); // hr tag is drawing a line
+
+//-----------------------------------//
+document.getElementById("title1").innerText="hoshitech.com";
+document.getElementById(2).innerHTML="<br>"+"welcome to hoshitech"; //html text
+document.getElementById(4).value=1402;
+
+
