818cb10d194d9ae09e98f11d2b74df5b3f487055
hanieh <h.khaleoghli@gmail.com>
2022-11-19 13:49:18 +0330

    Create SimpleJavaScriptDefineVariable.js

---
+++ 1/SimpleJavaScriptDefineVariable.js
@@ -0,0 +1,17 @@
+/*اینجا فهمیدم جاوا اسکریپت 4 تا نوع داده دارد
+java script has 4 data type
+
+boolean
+number
+string
+const
+undefined
+*/
+    var flag="True";
+    var num=450;
+    var sum=num+1;
+    const pi=3.14;
+    var name="mana";
+    var nume2;
+    alert(num);
+
