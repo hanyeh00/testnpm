a49a3c4daaf33b4b46a6184ab39d70f89294c749
hanyeh00 <h.khaleoghli@gmail.com>
2023-01-23 13:13:26 +0330

    show one item from dictionary

---
+++ FastApi/Book/returnOneBook.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app=FastAPI()
+BOOKS = {
+    'book_1': {'title': 'Atomic Habits', 'author': 'James Clear'},
+    'book_2': {'title': 'Ulysses', 'author': 'James Joyce'},
+    'book_3': {'title': 'To Kill a Mockingbird', 'author': ' Harper Lee'},
+    'book_4': {'title': 'Beloved', 'author': 'Toni Morrison'},
+    'book_5': {'title': 'Jane Eyre', 'author': 'Charlotte Brontë'},
+}
+@app.get("/books")
+def Books_name():
+    return BOOKS
+
+@app.get("/{one_book}")
+def Show_title(one_book):
+    return(BOOKS[one_book])
+
