2a23ca2d3ce789ee8f78f2fff243f4d259894af0
hanyeh00 <h.khaleoghli@gmail.com>
2023-02-02 16:25:43 +0330

    nothing special

---
+++ FastApi/Book/rt.py
@@ -0,0 +1,8 @@
+BOOKS = {
+    'book_1': {'title': 'Atomic Habits', 'author': 'James Clear'},
+    'book_2': {'title': 'Ulysses', 'author': 'James Joyce'},
+    'book_3': {'title': 'To Kill a Mockingbird', 'author': ' Harper Lee'},
+    'book_4': {'title': 'Beloved', 'author': 'Toni Morrison'},
+    'book_5': {'title': 'Jane Eyre', 'author': 'Charlotte Brontë'},
+}
+print(BOOKS.values())
